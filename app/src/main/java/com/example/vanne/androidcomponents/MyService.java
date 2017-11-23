60f631aba811866fc33fb6912dbe515118d1282e
vannesschancc <vannessc199365>
2017-11-22 18:32:21 -0800

    Initial commit

---
+++ app/src/main/java/com/example/vanne/androidcomponents/MyService.java
@@ -0,0 +1,53 @@
+package com.example.vanne.androidcomponents;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private static final String TAG = "MyService";
+    //Define a binder so that it can be used in onBind(),
+    private MyBinder mBinder = new MyBinder;
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.e(TAG, "onBind");
+        return mBinder;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG, "onBind");
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+
+    public class MyBinder extends Binder {
+        MyService.getService()
+    }
+}
+
